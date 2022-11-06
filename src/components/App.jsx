4c1e0c8a859a2c7e53b65ee6decda5632f4e2653
Mykola Hrechkosii <60157425+Hrechkosii-Nikolay@users.noreply.github.com>
2022-11-06 18:00:01 +0200

    change state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,22 @@ import Filter from './Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    if (contacts) {
+      this.setState({ contacts: JSON.parse(contacts) });
+    }
+  }
 
   addContacts = ({ name, number }) => {
     const existName = this.state.contacts.find(el => el.name === name);
